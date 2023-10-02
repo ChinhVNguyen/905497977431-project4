1515166c4862f27fcee0d373ca2f703ad77bcb04
ChinhVNguyen <chinhfptusa@gmail.com>
2023-10-02 09:24:25 +0700

    Add validator request

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'ot35ha3cgg'
+const apiId = '7d9qzq63i7'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
